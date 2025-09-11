b0b13a6efd0e901e867381ce699d053d1c153a15
Maykol-23 <maykol.litari@tecsup.edu.pe>
2025-09-11 12:48:37 -0500

    feat:change text "Hello"

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Maykol estas haciendo este nuevo commmit!",
         modifier = modifier
     )
 }
